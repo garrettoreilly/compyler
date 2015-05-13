89bc1b2f46bb547e7c825efdd12b5915e2d2e95f
Garrett Oreilly <garrettoreilly@icloud.com>
2015-05-13 17:32:18 -0400

    Fixed EOF and Token sillyness

--- compile.py
+++ compile.py
@@ -1,12 +1,11 @@
-import sys, lex
+import sys, lexer
 
 def main():
     f = open(sys.argv[1], 'r')
     program = f.read()
     f.close()
-    tokenList = lex.lexProgram(program)
-    for i in tokenList:
-        print(i.kind);
+    tokenList = lexer.lex_program(program)
+    print("Compilation successful!")
 
 if __name__ == '__main__':
     main()
--- lexer.py
+++ lexer.py
@@ -1,78 +1,89 @@
 import sys, string
 
-class Token:
-    def __init__(self, kind, value, line=None, position=None):
-        self.kind = kind
-        self.value = value
-        self.line = line
-        self.position = position
-
-def lexProgram(program):
-    tokenList = []
+def lex_program(program):
+    token_list = []
     line = 1
     position = 0
     str = False
-    keyword_cache = ""
     i = 0
     while i < len(program):
         if str: 
-            str = validateString(program[i])
+            str = validate_string(program[i])
         elif program[i] in "(){}+": 
-            tokenList.append(singleCharTokens(program[i]))
-            tokenList[-1].line = line
-            tokenList[-1].position = position
+            token_list.append(single_char_tokens(program[i]))
+            token_list[-1] += (line, position)
         elif i+1 < len(program) and program[i]+program[i+1] == "!=": 
-            tokenList.append(Token("BoolOp", "!=", line, position))
+            token_list.append(("BoolOp", "!=", line, position))
             i+=1
         elif i+1 < len(program) and program[i]+program[i+1] == "==": 
-            tokenList.append(Token("BoolOp", "==", line, position))
+            token_list.append(("BoolOp", "==", line, position))
             i+=1
         elif program[i] == "=": 
-            tokenList.append(Token("AssignOp", "=", line, position))
+            token_list.append(("AssignOp", "=", line, position))
         elif program[i] == "\"": 
             str = True
         elif program[i] in "0123456789": 
-            tokenList.append(Token("Digit", program[i], line, position))
+            token_list.append(("Digit", program[i], line, position))
         elif program[i] in string.ascii_lowercase:
-            if i+1 < len(program) and program[i+1] not in string.ascii_lowercase:
-                tokenList.append(check_keyword(keyword_cache))
-                tokenList[-1].line = line
-                tokenList[-1].position = position
-            else: keyword_cache+=program[i]
+            new_token = check_keyword(program[i:])
+            new_token += (line, position)
+            token_list.append(new_token)
+            i+=(len(new_token[1])-1)
         elif program[i] == " ":
             pass
         elif program[i] == "\n":
             line+=1
             position = 1
         elif program[i] == "$":
-            tokenList.append(Token("EOF", "$", line, position))
-            if i != len(program) - 1 and program[i+1] != '\n':
+            token_list.append(("EOF", "$", line, position))
+            if program[i+1:] != [] and program[i+1:] != "\n":
                 print("Warning! EOF out of nowhere!");
+            return token_list
         position+=1
         i+=1
-    return tokenList
 
-def validateString(c):
+    if token_list[-1][1] != "$":
+        print("Warning! No EOF in file!")
+        token_list.append(("EOF", "$", line, position))
+
+    return token_list
+
+def validate_string(c):
     if c == "\"":
         return False
     elif c not in string.ascii_lowercase:
         sys.exit("Error!", c, "is not a valid string character!")
     else:
-        return true
+        return True
 
-def singleCharTokens(c):
-    if c == "(": return Token("OpenParen", "(")
-    if c == ")": return Token("CloseParen", ")")
-    if c == "{": return Token("OpenBrace", "{")
-    if c == "}": return Token("CloseBrace", "}")
-    if c == "+": return Token("IntOp", "+")
+def single_char_tokens(c):
+    if c == "(": 
+        return ("OpenParen", "(")
+    if c == ")": 
+        return ("CloseParen", ")")
+    if c == "{": 
+        return ("OpenBrace", "{")
+    if c == "}": 
+        return ("CloseBrace", "}")
+    if c == "+": 
+        return ("IntOp", "+")
 
-def check_keyword(cache):
-    if cache in ["boolean", "string", "int"]: kind = "IdType"
-    elif cache in ["true", "false"]: kind = "Boolean"
-    elif cache == "if": kind = "If"
-    elif cache == "while": kind = "While"
-    elif cache == "print": kind = "Print"
-    elif len(cache) == 1: kind = "Id"
-    else: sys.exit("Error! Not a valid keyword!")
-    return Token(kind, cache)
+def check_keyword(program):
+    if program[:7] == "boolean":
+        return ("IdType", "boolean")
+    elif program[:6] == "string":
+        return ("IdType", "string")
+    elif program[:5] == "while":
+        return ("While", "while")
+    elif program[:5] == "print":
+        return ("Print", "print")
+    elif program[:5] == "false":
+        return ("Boolean", "false")
+    elif program[:4] == "true":
+        return ("Boolean", "ture")
+    elif program[:3] == "int":
+        return ("IdType", "int")
+    elif program[:2] == "if":
+        return ("If", "if")
+    else:
+        return ("Id", program[0])
