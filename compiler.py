3671e98d41fa695db7b51ab9bdadc8c6ea6d7fc8
Garrett Oreilly <garrettoreilly@icloud.com>
2015-05-13 20:50:18 -0400

    Added parser functions

--- compiler.py
+++ compiler.py
@@ -1,10 +1,11 @@
-import sys, lexer
+import sys, lexer, parser
 
 def main():
     f = open(sys.argv[1], 'r')
     program = f.read()
     f.close()
-    tokenList = lexer.lex_program(program)
+    token_list = lexer.lex_program(program)
+    parser.parse_program(token_list)
     print("Compilation successful!")
 
 if __name__ == '__main__':
--- lexer.py
+++ lexer.py
@@ -4,86 +4,94 @@ def lex_program(program):
     token_list = []
     line = 1
     position = 0
-    str = False
+    char_list = False
+    cl_start = 0
     i = 0
     while i < len(program):
-        if str: 
-            str = validate_string(program[i])
+        if char_list: 
+            char_list = validate_string(program[i])
+            if not char_list:
+                token_list.append({"type": "CharList", "value": program[cl_start:i+1],
+                        "line": line, "position": position - (i+1 - cl_start)})
         elif program[i] in "(){}+": 
             token_list.append(single_char_tokens(program[i]))
-            token_list[-1] += (line, position)
+            token_list[-1]["line"] = line
+            token_list[-1]["position"] = position
         elif i+1 < len(program) and program[i]+program[i+1] == "!=": 
-            token_list.append(("BoolOp", "!=", line, position))
+            token_list.append({"type": "BoolOp", "value": "!=", "line": line, "position": position})
             i+=1
         elif i+1 < len(program) and program[i]+program[i+1] == "==": 
-            token_list.append(("BoolOp", "==", line, position))
+            token_list.append({"type": "BoolOp", "value": "==", "line": line, "position": position})
             i+=1
         elif program[i] == "=": 
-            token_list.append(("AssignOp", "=", line, position))
+            token_list.append({"type": "AssignOp", "value": "=", "line": line, "position": position})
         elif program[i] == "\"": 
-            str = True
+            char_list = True
+            cl_start = i
         elif program[i] in "0123456789": 
-            token_list.append(("Digit", program[i], line, position))
+            token_list.append({"type": "Digit", "value": program[i], "line": line,
+                "position": position})
         elif program[i] in string.ascii_lowercase:
             new_token = check_keyword(program[i:])
-            new_token += (line, position)
+            new_token["line"] = line
+            new_token["position"] = position
             token_list.append(new_token)
-            i+=(len(new_token[1])-1)
+            i+=(len(new_token["value"])-1)
         elif program[i] == " ":
             pass
         elif program[i] == "\n":
             line+=1
             position = 1
         elif program[i] == "$":
-            token_list.append(("EOF", "$", line, position))
+            token_list.append({"type": "EOF", "value": "$", "line": line, "position": position})
             if program[i+1:] != [] and program[i+1:] != "\n":
                 print("Warning! EOF out of nowhere!");
             return token_list
         position+=1
         i+=1
 
-    if token_list[-1][1] != "$":
+    if token_list[-1]["value"] != "$":
         print("Warning! No EOF in file!")
-        token_list.append(("EOF", "$", line, position))
+        token_list.append({"type": "EOF", "value": "$", "line": line, "position": position})
 
     return token_list
 
 def validate_string(c):
     if c == "\"":
         return False
-    elif c not in string.ascii_lowercase:
+    elif c not in string.ascii_lowercase and c != " ":
         sys.exit("Error!", c, "is not a valid string character!")
     else:
         return True
 
 def single_char_tokens(c):
     if c == "(": 
-        return ("OpenParen", "(")
+        return {"type": "OpenParen", "value": "("}
     if c == ")": 
-        return ("CloseParen", ")")
+        return {"type": "CloseParen", "value": ")"}
     if c == "{": 
-        return ("OpenBrace", "{")
+        return {"type": "OpenBrace", "value": "{"}
     if c == "}": 
-        return ("CloseBrace", "}")
+        return {"type": "CloseBrace", "value": "}"}
     if c == "+": 
-        return ("IntOp", "+")
+        return {"type": "IntOp", "value": "+"}
 
 def check_keyword(program):
     if program[:7] == "boolean":
-        return ("IdType", "boolean")
+        return {"type": "IdType", "value": "boolean"}
     elif program[:6] == "string":
-        return ("IdType", "string")
+        return {"type": "IdType", "value": "string"}
     elif program[:5] == "while":
-        return ("While", "while")
+        return {"type": "While", "value":"while"}
     elif program[:5] == "print":
-        return ("Print", "print")
+        return {"type": "Print", "value": "print"}
     elif program[:5] == "false":
-        return ("Boolean", "false")
+        return {"type": "Boolean", "value": "false"}
     elif program[:4] == "true":
-        return ("Boolean", "ture")
+        return {"type": "Boolean", "value": "true"}
     elif program[:3] == "int":
-        return ("IdType", "int")
+        return {"type": "IdType", "value": "int"}
     elif program[:2] == "if":
-        return ("If", "if")
+        return {"type": "If", "value": "if"}
     else:
-        return ("Id", program[0])
+        return {"type": "Id", "value": program[0]}
