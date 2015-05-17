407a6ec48b58c1490f4d03acff8ecd849cbe72f4
Garrett Oreilly <garrettoreilly@icloud.com>
2015-05-16 21:47:27 -0400

    Updated variable names

--- compiler.py
+++ compiler.py
@@ -6,8 +6,8 @@ def main():
     f.close()
     token_list = lexer.lex_program(program)
     parser.parse_program(token_list)
-    tree = cst.CST({"type": "Program"})
-    tree.generate_cst(token_list)
+    cst_root = cst.CST({"type": "Program"})
+    cst_root.generate_cst(token_list)
     print("Compilation successful!")
 
 if __name__ == '__main__':
--- cst.py
+++ cst.py
@@ -20,118 +20,113 @@ class CST:
         self.print_cst(1)
 
     def parse_block(self, token_list):
-        cst = CST({"type": "Block"})
-        cst.children += [CST(token_list.pop(0))]
-        token_list = cst.parse_statement_list(token_list)
-        # if result is not None:
-        #     self.children += [result]
-        cst.children += [CST(token_list.pop(0))]
-        self.children += [cst]
+        node = CST({"type": "Block"})
+        node.children += [CST(token_list.pop(0))]
+        token_list = node.parse_statement_list(token_list)
+        node.children += [CST(token_list.pop(0))]
+        self.children += [node]
         return token_list
 
     def parse_statement_list(self, token_list):
-        cst = CST({"type": "Statement List"})
+        node = CST({"type": "Statement List"})
         if token_list[0]["type"] is "CloseBrace":
             return token_list
-        token_list = cst.parse_statement(token_list)
-        # self.children += [result]
-        token_list = cst.parse_statement_list(token_list)
-        # if result is not None:
-        #     self.children += [result]
-        self.children += [cst]
+        token_list = node.parse_statement(token_list)
+        token_list = node.parse_statement_list(token_list)
+        self.children += [node]
         return token_list
 
     def parse_statement(self, token_list):
-        cst = CST({"type": "Statement"})
+        node = CST({"type": "Statement"})
         if token_list[0]["type"] is "Print":
-            token_list = cst.parse_print_statement(token_list)
+            token_list = node.parse_print_statement(token_list)
         elif token_list[0]["type"] is "Id":
-            token_list = cst.parse_assign_statement(token_list)
+            token_list = node.parse_assign_statement(token_list)
         elif token_list[0]["type"] is "IdType":
-            token_list = cst.parse_var_decl(token_list)
+            token_list = node.parse_var_decl(token_list)
         elif token_list[0]["type"] is "While":
-            token_list = cst.parse_while_statement(token_list)
+            token_list = node.parse_while_statement(token_list)
         elif token_list[0]["type"] is "If":
-            token_list = cst.parse_if_statement(token_list)
+            token_list = node.parse_if_statement(token_list)
         elif token_list[0]["type"] is "OpenBrace":
-            token_list = cst.parse_block(token_list)
-        self.children += [cst]
+            token_list = node.parse_block(token_list)
+        self.children += [node]
         return token_list
 
     def parse_print_statement(self, token_list):
-        cst = CST({"type": "Print Statement"})
-        cst.children += [CST(token_list.pop(0)), CST(token_list.pop(0))]
-        token_list = cst.parse_expr(token_list)
-        cst.children += [CST(token_list.pop(0))]
-        self.children += [cst]
+        node = CST({"type": "Print Statement"})
+        node.children += [CST(token_list.pop(0)), CST(token_list.pop(0))]
+        token_list = node.parse_expr(token_list)
+        node.children += [CST(token_list.pop(0))]
+        self.children += [node]
         return token_list
 
     def parse_assign_statement(self, token_list):
-        cst = CST({"type": "Assignment Statement"})
-        cst.children += [CST(token_list.pop(0)), CST(token_list.pop(0))]
-        token_list = cst.parse_expr(token_list)
-        self.children += [cst]
+        node = CST({"type": "Assignment Statement"})
+        node.children += [CST(token_list.pop(0)), CST(token_list.pop(0))]
+        token_list = node.parse_expr(token_list)
+        self.children += [node]
         return token_list
 
     def parse_var_decl(self, token_list):
-        cst = CST({"type": "Variable Declaration"})
-        cst.children += [CST(token_list.pop(0)), CST(token_list.pop(0))]
-        self.children += [cst]
+        node = CST({"type": "Variable Declaration"})
+        node.children += [CST(token_list.pop(0)), CST(token_list.pop(0))]
+        self.children += [node]
         return token_list
 
     def parse_while_statement(self, token_list):
-        cst = CST({"type": "While Statement"})
-        cst.children += [CST(token_list.pop(0))]
-        token_list = cst.parse_bool_expr(token_list)
-        token_list = cst.parse_block(token_list)
-        self.children += [cst]
+        node = CST({"type": "While Statement"})
+        node.children += [CST(token_list.pop(0))]
+        token_list = node.parse_bool_expr(token_list)
+        token_list = node.parse_block(token_list)
+        self.children += [node]
         return token_list
 
     def parse_if_statement(self, token_list):
-        cst = CST({"type": "If Statement"})
-        cst.children += [CST(token_list.pop(0))]
-        token_list = cst.parse_bool_expr(token_list)
-        token_list = cst.parse_block(token_list)
-        self.children += [cst]
+        node = CST({"type": "If Statement"})
+        node.children += [CST(token_list.pop(0))]
+        token_list = node.parse_bool_expr(token_list)
+        token_list = node.parse_block(token_list)
+        self.children += [node]
         return token_list
 
     def parse_expr(self, token_list):
-        cst = CST({"type": "Expression"})
+        node = CST({"type": "Expression"})
         if token_list[0]["type"] is "Digit":
-            token_list = cst.parse_int_expr(token_list)
+            token_list = node.parse_int_expr(token_list)
         elif token_list[0]["type"] is "CharList":
-            token_list = cst.parse_string_expr(token_list)
+            token_list = node.parse_string_expr(token_list)
         elif token_list[0]["type"] in ["OpenParen", "BoolVal"]:
-            token_list = cst.parse_bool_expr(token_list)
+            token_list = node.parse_bool_expr(token_list)
         elif token_list[0]["type"] is "Id":
-            cst.children += [CST(token_list.pop(0))]
-        self.children += [cst]
+            node.children += [CST(token_list.pop(0))]
+        self.children += [node]
         return token_list
 
     def parse_int_expr(self, token_list):
-        cst = CST({"type": "Int Expression"})
-        cst.children += [CST(token_list.pop(0))]
+        node = CST({"type": "Int Expression"})
+        node.children += [CST(token_list.pop(0))]
         if token_list[0]["type"] is "IntOp":
-            cst.children += [CST(token_list.pop(0))]
-            token_list = cst.parse_expr(token_list)
-        self.children += [cst]
+            node.children += [CST(token_list.pop(0))]
+            token_list = node.parse_expr(token_list)
+        self.children += [node]
         return token_list
 
     def parse_string_expr(self, token_list):
-        cst = CST({"type": "String Expression"})
-        cst.children += [CST(token_list.pop(0))]
-        self.children += [cst]
+        node = CST({"type": "String Expression"})
+        node.children += [CST(token_list.pop(0))]
+        self.children += [node]
         return token_list
 
     def parse_bool_expr(self, token_list):
-        cst = CST({"type": "Boolean Expression"})
+        node = CST({"type": "Boolean Expression"})
         if token_list[0]["type"] is "OpenParen":
-            cst.children += [CST(token_list.pop(0))]
-            token_list = cst.parse_expr(token_list)
-            cst.children += [CST(token_list.pop(0))]
-            token_list = cst.parse_expr(token_list)
-            cst.children += [CST(token_list.pop(0))]
+            node.children += [CST(token_list.pop(0))]
+            token_list = node.parse_expr(token_list)
+            node.children += [CST(token_list.pop(0))]
+            token_list = node.parse_expr(token_list)
+            node.children += [CST(token_list.pop(0))]
         else:
-            cst.children += [CST(token_list.pop(0))]
-        self.children += [cst]
+            node.children += [CST(token_list.pop(0))]
+        self.children += [node]
         return token_list
